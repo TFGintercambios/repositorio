c05fe0696d8eacf441b086f2e908c6e5e9fc8ee5
josdurgar1 <josdurgar1@alum.us.es>
2017-04-20 13:06:33 +0200

    Creación Proyecto Base
    Creación del proyecto base

---
+++ com.intercambios.app/src/pages/home/home.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ LatLng,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps) {
+
+  }
+ngAfterViewInit() {
+ this.loadMap();
+}
+
+loadMap() {
+
+ let element: HTMLElement = document.getElementById('map');
+
+ let map: GoogleMap = this.googleMaps.create(element);
+
+ // listen to MAP_READY event
+ // You must wait for this event to fire before adding something to the map or modifying it in anyway
+ map.one(GoogleMapsEvent.MAP_READY).then(() => console.log('Map is ready!'));
+
+ // create LatLng object
+ let ionic: LatLng = new LatLng(43.0741904,-89.3809802);
+
+ // create CameraPosition
+ let position: CameraPosition = {
+   target: ionic,
+   zoom: 18,
+   tilt: 30
+ };
+
+ // move the map's camera to position
+ map.moveCamera(position);
+
+ // create new marker
+ let markerOptions: MarkerOptions = {
+   position: ionic,
+   title: 'Ionic'
+ };
+
+ }
+}
